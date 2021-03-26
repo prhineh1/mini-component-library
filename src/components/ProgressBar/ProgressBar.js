9ca2739e1ed5860a3c643c2f2b5263c58119d540
Phillip Rhinehart <C-Phillip.Rhinehart@charter.com>
2021-03-25 21:28:43 -0400

    progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,47 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return <ProgressBarBase max="100" size={size} value={value} role="progressbar" aria-valuenow={value} aria-valuemin="0" aria-valuemax="100">{value}</ProgressBarBase>;
 };
+
+const ProgressBarBase = styled.progress`
+  &[value] {
+    appearance: none;
+    width: 370px;
+    height: ${p => {
+      switch (p.size) {
+        case 'large':
+          return '24px';
+        case 'small':
+          return '8px';
+        case 'medium':
+        default:
+          return '12px';
+      }
+    }}
+  }
+
+  &::-webkit-progress-bar {
+    box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+    background-color: ${COLORS.transparentGray15};
+    border-radius: ${p => p.size === 'large' ? '8px' : '4px'};
+    padding: ${p => p.size === 'large' && '4px'};
+  }
+
+  &::-webkit-progress-value {
+    background-color: ${COLORS.primary};
+    border-radius: ${p => {
+      /*
+        incrementally round the top-right and bottom-right
+        corners as the progress bar nears completion
+      */
+      if (p.value > 99) {
+        const percentPixel = (1 - (100 - p.value)) * 4;
+        return `4px ${percentPixel}px ${percentPixel}px 4px`;
+      }
+      return `4px 0 0 4px`;
+    }};
+  }
+`;
 
 export default ProgressBar;
